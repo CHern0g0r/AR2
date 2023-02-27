6235ddb82482963b780dea052bfc6ea6db23549f
root <chernogorsky.fedor@huawei-partners.com>
2023-02-27 16:21:37 +0800

    Add launch order and refine ranker scripts

---
+++ AR2/scripts/trivia/order.md
@@ -0,0 +1,6 @@
+Launch order:
+- warm_retr.sh
+- eval_retr_gen_hard.sh
+- reform_to_ce_rank.sh
+- warm_rank.sh
+- train_ar2.sh
--- AR2/scripts/trivia/reform_to_ce_rank.sh
+++ AR2/scripts/trivia/reform_to_ce_rank.sh
@@ -6,13 +6,13 @@ CKPT_NUM=40000
 OUT_DIR=./output/$EXP_NAME
 TOPK_FILE=$OUT_DIR/$CKPT_NUM/dev_result_dict_list.json # dev_result_dict_list.json is generate in previous step
 CE_TRAIN_FILE=$OUT_DIR/$CKPT_NUM/dev_ce_0_triviaqa.json
-LABELED_FILE=$OUT_DIR/$CKPT_NUM/biencoder-trivia-dev.json
+LABELED_FILE=$DATA_DIR/biencoder-trivia-dev.json
 
 python ./utils/prepare_ce_data.py $TOPK_FILE $CE_TRAIN_FILE $LABELED_FILE   # generate dev set file
 
 
 TOPK_FILE=$OUT_DIR/$CKPT_NUM/train_result_dict_list.json # train_result_dict_list.json is generate in previous step
 CE_TRAIN_FILE=$OUT_DIR/$CKPT_NUM/train_ce_0_triviaqa.json
-LABELED_FILE=$OUT_DIR/$CKPT_NUM/biencoder-trivia-train.json
+LABELED_FILE=$DATA_DIR/biencoder-trivia-train.json
 
 python ./utils/prepare_ce_data.py $TOPK_FILE $CE_TRAIN_FILE $LABELED_FILE # generate train set file
